436384bdbbaa8a1996afb8f643f52b67f9c20df6
mp250444 <71600230+mp250444@users.noreply.github.com>
2020-11-18 22:46:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
   <div id="app">
      <toolbarItem></toolbarItem>
-     <router-view :some-value-to-pass="localValue" :btn-clicked-value="changeInfo"></router-view> 
+     <router-view></router-view> 
     
   </div>
 </template>
   
 <script>
 import toolbarItem from '@/components/toolbarItem.vue';
-import { eventBus } from './main';
+ 
 
 export default {
   components: {
     'toolbarItem':toolbarItem,
   },
-  data() {
-    return {
-      localValue:[],
-      changeInfo:false,
-    }
-  },
 
-  created () {
-
-   
-    eventBus.$on('task_information', (data)=> {
-      
-       this.localValue.push(data);
-      //  console.log(this.localValue)
-       
-    });
-
-      eventBus.$on('isClicked', (data)=> {
-      
-       this.changeInfo = data;
-       console.log(this.changeInfo)
-       
-    });
-
-    
-  },
-  
 }
 </script>
 
